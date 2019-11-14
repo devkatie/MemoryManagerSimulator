991d1712ceea01ab3ba23f4fc3ee12d07c8f34d9
Katie <portk22@mail.sunysuffolk.edu>
2019-11-13 21:21:54 -0500

    getting the ball rollin...

---
+++ MemManager/src/application/GUIController.java
@@ -0,0 +1,26 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+public class GUIController {
+
+	@FXML private ComboBox<?> algorithmComboBox;
+
+	@FXML private Button compactMemBtn;
+
+	@FXML private Button addMemBtn;
+
+	@FXML private Button removeMemBtn;
+
+	@FXML private TextField totalMemTextField;
+
+	@FXML private TextField osMemTextField;
+
+	@FXML private TextField processSizeTextField;
+
+	@FXML private ComboBox<?> pidComboBox;
+
+}
---
+++ MemManager/src/application/MainApp.java
@@ -0,0 +1,53 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    private Stage primaryStage;
+    private VBox rootLayout;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Memory Manager");
+        this.primaryStage.getIcons().add(new Image("https://img7.androidappsapk.co/300/a/f/7/com.smartprojects.RAMOptimization.png"));
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(MainApp.class.getResource("GUI.fxml"));
+        Parent root = null;
+		try {
+			root = loader.load();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        
+     // Show the scene
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+  
+    /**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
